d9c631f9557aa95a14e70a7cf3bf5a573c05c0ac
Maksim Shiryaev <shimady563@gmail.com>
2024-08-23 14:57:23 +0300

    feat(security): make user implement user details

--- tracker/src/main/java/com/shimady/tracker/model/User.java
+++ tracker/src/main/java/com/shimady/tracker/model/User.java
@@ -3,11 +3,17 @@ package com.shimady.tracker.model;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
 
 @Getter
 @Setter
 @Entity
-public class User {
+public class User implements UserDetails {
     @Id
     @GeneratedValue
     private Long id;
@@ -19,4 +25,29 @@ public class User {
     private String email;
 
     private String phoneNumber;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("USER"));
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
