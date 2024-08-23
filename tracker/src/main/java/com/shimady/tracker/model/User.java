71eaa4fa7b39b2143eeb331a3c906dd0f65eb779
Maksim Shiryaev <shimady563@gmail.com>
2024-08-23 15:35:08 +0300

    Revert "feat(security): make user implement user details"
    This reverts commit d9c631f9557aa95a14e70a7cf3bf5a573c05c0ac.

--- tracker/src/main/java/com/shimady/tracker/model/User.java
+++ tracker/src/main/java/com/shimady/tracker/model/User.java
@@ -3,17 +3,11 @@ package com.shimady.tracker.model;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
-
-import java.util.Collection;
-import java.util.List;
 
 @Getter
 @Setter
 @Entity
-public class User implements UserDetails {
+public class User {
     @Id
     @GeneratedValue
     private Long id;
@@ -25,29 +19,4 @@ public class User implements UserDetails {
     private String email;
 
     private String phoneNumber;
-
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of(new SimpleGrantedAuthority("USER"));
-    }
-
-    @Override
-    public boolean isAccountNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isAccountNonLocked() {
-        return true;
-    }
-
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isEnabled() {
-        return true;
-    }
 }
