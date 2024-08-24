77163bdf6f812507f9cb24a61958193fab9925ec
Maksim Shiryaev <shimady563@gmail.com>
2024-08-24 22:42:14 +0300

    feat(errors): add access denied exception handling

--- tracker/src/main/java/com/shimady/tracker/exception/handler/GlobalExceptionHandler.java
+++ tracker/src/main/java/com/shimady/tracker/exception/handler/GlobalExceptionHandler.java
@@ -1,8 +1,29 @@
 package com.shimady.tracker.exception.handler;
 
+import com.shimady.tracker.exception.AccessDeniedException;
+import com.shimady.tracker.exception.AppError;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import java.time.LocalDateTime;
+
+@Slf4j
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
+    @ExceptionHandler(AccessDeniedException.class)
+    public ResponseEntity<AppError> handleAccessDeniedException(AccessDeniedException e) {
+        log.error(e.getMessage());
+        return new ResponseEntity<>(
+                new AppError(
+                        e.getMessage(),
+                        HttpStatus.FORBIDDEN.value(),
+                        LocalDateTime.now()
+                ),
+                HttpStatus.FORBIDDEN
+        );
+    }
 }
