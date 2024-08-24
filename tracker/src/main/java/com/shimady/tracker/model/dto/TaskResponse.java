d660b14da81bf5a291aa97721bb28a81a1ca095d
Maksim Shiryaev <shimady563@gmail.com>
2024-08-24 22:43:27 +0300

    feat(tasks): implement some operations on tasks
    implement task creation, task update, search for tasks by users and ability to sort and page data

---
+++ tracker/src/main/java/com/shimady/tracker/model/dto/TaskResponse.java
@@ -0,0 +1,22 @@
+package com.shimady.tracker.model.dto;
+
+import com.shimady.tracker.model.TaskStatus;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class TaskResponse {
+    private Long id;
+    public String title;
+    public String description;
+    public TaskStatus status;
+    public LocalDateTime createdAt;
+    public LocalDateTime deadline;
+}
---
+++ tracker/src/main/java/com/shimady/tracker/service/TaskService.java
@@ -0,0 +1,76 @@
+package com.shimady.tracker.service;
+
+import com.shimady.tracker.exception.ResourceNotFoundException;
+import com.shimady.tracker.model.Task;
+import com.shimady.tracker.model.User;
+import com.shimady.tracker.model.dto.TaskCreationRequest;
+import com.shimady.tracker.model.dto.TaskResponse;
+import com.shimady.tracker.model.dto.TaskUpdateRequest;
+import com.shimady.tracker.model.sort.TaskSort;
+import com.shimady.tracker.repository.TaskRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.modelmapper.ModelMapper;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Slice;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class TaskService {
+
+    private final TaskRepository taskRepository;
+    private final ModelMapper modelMapper;
+
+    @Transactional
+    public void createTask(TaskCreationRequest request) {
+        User user = (User) SecurityContextHolder
+                .getContext()
+                .getAuthentication()
+                .getPrincipal();
+
+        log.info("Creating task for user with id {}", user.getId());
+
+        Task task = modelMapper.map(request, Task.class);
+        task.setUser(user);
+        taskRepository.save(task);
+    }
+
+    @Transactional
+    public void updateTask(Long id, TaskUpdateRequest request) {
+        log.info("Updating task with id {}", id);
+
+        Task task = getTaskById(id);
+
+        task.setTitle(request.getTitle());
+        task.setDescription(request.getDescription());
+        task.setDeadline(request.getDeadline());
+        task.setStatus(request.getStatus());
+
+        taskRepository.save(task);
+    }
+
+    @Transactional
+    public Slice<TaskResponse> getAllTasksByUser(int limit, int offset, TaskSort sort) {
+        User user = (User) SecurityContextHolder
+                .getContext()
+                .getAuthentication()
+                .getPrincipal();
+
+        Slice<Task> tasks = taskRepository.findAllByUser(
+                user,
+                PageRequest.of(offset, limit, sort.getSortValue())
+        );
+
+        return tasks.map((element) -> modelMapper.map(element, TaskResponse.class));
+    }
+
+    private Task getTaskById(Long id) {
+        return taskRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Task with id " + id + " not found"));
+    }
+
+}
