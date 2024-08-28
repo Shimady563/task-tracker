9a401c2a351703d36bd18444d6ab634a1fa5585f
Maksim Shiryaev <shimady563@gmail.com>
2024-08-28 23:04:59 +0300

    feat(config): create topic for each message source

---
+++ tracker/src/main/java/com/shimady/tracker/config/KafkaTopic.java
@@ -0,0 +1,34 @@
+package com.shimady.tracker.config;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+
+@Configuration
+public class KafkaTopic {
+
+    @Bean
+    public NewTopic emailTopic() {
+        return TopicBuilder.name("emailTopic")
+                .partitions(4)
+                .replicas(1)
+                .build();
+    }
+
+    @Bean
+    public NewTopic smsTopic() {
+        return TopicBuilder.name("smsTopic")
+                .partitions(4)
+                .replicas(1)
+                .build();
+    }
+
+    @Bean
+    public NewTopic pushTopic() {
+        return TopicBuilder.name("pushTopic")
+                .partitions(4)
+                .replicas(1)
+                .build();
+    }
+}
