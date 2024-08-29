e98a6f3a657fb8421a0d0b96b0005475f0981b9f
Maksim Shiryaev <shimady563@gmail.com>
2024-08-29 19:34:42 +0300

    feat(messaging): implement push message sending on user authentication

--- tracker/src/main/java/com/shimady/tracker/event/MessagingEventListener.java
+++ tracker/src/main/java/com/shimady/tracker/event/MessagingEventListener.java
@@ -1,14 +1,19 @@
 package com.shimady.tracker.event;
 
 import com.shimady.tracker.exception.ResourceNotFoundException;
+import com.shimady.tracker.model.TaskStatus;
 import com.shimady.tracker.model.User;
 import com.shimady.tracker.model.messaging.EmailMessage;
+import com.shimady.tracker.model.messaging.PushMessage;
 import com.shimady.tracker.model.messaging.SMSMessage;
+import com.shimady.tracker.repository.TaskRepository;
 import com.shimady.tracker.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.event.EventListener;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.scheduling.annotation.Async;
+import org.springframework.security.authentication.event.AuthenticationSuccessEvent;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.event.TransactionPhase;
 import org.springframework.transaction.event.TransactionalEventListener;
@@ -18,6 +23,7 @@ import org.springframework.transaction.event.TransactionalEventListener;
 @RequiredArgsConstructor
 public class MessagingEventListener {
 
+    private final TaskRepository taskRepository;
     private final UserRepository userRepository;
     private final KafkaTemplate<String, Object> kafkaTemplate;
 
@@ -33,5 +39,18 @@ public class MessagingEventListener {
 
         kafkaTemplate.send("emailTopic", emailMessage);
         kafkaTemplate.send("smsTopic", smsMessage);
+    }
+
+    @Async
+    @EventListener(value = AuthenticationSuccessEvent.class)
+    public void onAuthenticationSuccessEvent(AuthenticationSuccessEvent event) {
+        User user = (User) event.getAuthentication().getPrincipal();
+
+        log.info("Received authentication success event, email {}", user.getEmail());
+
+        long tasksToDo = taskRepository.countByUserAndStatus(user, TaskStatus.TODO);
+        PushMessage pushMessage = new PushMessage(user.getUsername(), tasksToDo);
+
+        kafkaTemplate.send("pushTopic", pushMessage);
     }
 }
