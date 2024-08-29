cc83b3e4bfe8c384e95e253595e49e704d526a1c
Maksim Shiryaev <shimady563@gmail.com>
2024-08-29 19:45:52 +0300

    feat(tracker): add name field for task status

--- tracker/src/main/java/com/shimady/tracker/model/TaskStatus.java
+++ tracker/src/main/java/com/shimady/tracker/model/TaskStatus.java
@@ -1,7 +1,14 @@
 package com.shimady.tracker.model;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public enum TaskStatus {
-    TODO,
-    DEFERRED,
-    DONE
+    TODO("To Do"),
+    DEFERRED("Deferred"),
+    DONE("Done");
+
+    private final String name;
 }
