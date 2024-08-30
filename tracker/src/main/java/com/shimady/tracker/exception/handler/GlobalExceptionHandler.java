f0c7a57a1c7d7fe743600bb7b217cefd05b74379
Maksim Shiryaev <shimady563@gmail.com>
2024-08-30 14:30:32 +0300

    feat(errors): add default spring data exceptions handling

--- tracker/src/main/java/com/shimady/tracker/exception/handler/GlobalExceptionHandler.java
+++ tracker/src/main/java/com/shimady/tracker/exception/handler/GlobalExceptionHandler.java
@@ -4,6 +4,7 @@ import com.shimady.tracker.exception.AccessDeniedException;
 import com.shimady.tracker.exception.AppError;
 import com.shimady.tracker.exception.ResourceNotFoundException;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -39,5 +40,25 @@ public class GlobalExceptionHandler {
                 ),
                 HttpStatus.NOT_FOUND
         );
+    }
+
+    @ExceptionHandler(DataAccessException.class)
+    public ResponseEntity<AppError> handleDataAccessException(DataAccessException e) {
+        log.error(e.getMessage());
+        return new ResponseEntity<>(
+                new AppError(
+                        buildDataExceptionMessage(e.getMostSpecificCause().getMessage()),
+                        HttpStatus.CONFLICT.value(),
+                        LocalDateTime.now()
+                ),
+                HttpStatus.CONFLICT
+        );
+    }
+
+    private String buildDataExceptionMessage(String message) {
+        if (message.contains("Detail:")) {
+            return message.split("Detail:")[1].trim();
+        }
+        return message;
     }
 }
